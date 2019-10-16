95e5bf52778b7766ca6f00a059dbb324c2fc5ab9
Maria Ludmilla <maria.ludmilla.so@hotmail.com>
2019-10-16 00:21:13 -0300

    second commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# web_ufma
+ Trabalhos para a disciplina Desenvolvimento WEB. Do curso de Engenharia da Computação - UFMA
---
+++ javascript/scriptJS.js
@@ -0,0 +1,15 @@
+   function loginA(){
+    
+    var username=document.getElementById('uname').value;
+    username=username.toLowerCase();
+    var password=document.getElementById('psw').value;
+    password=password.toLowerCase();
+    
+
+    if (username=="admin" && password=="admin") { 
+        window.location.replace("./admin.html");}
+    else if (username=="outro" && password=="outrosenha") { window.location.href="./admin.html";  }
+    else if (username=="outro" && password=="outrasenha") {  window.location.href="./admin.html"; }
+    else { alert("Senha ou Usuário inválido."); }
+    
+    }
