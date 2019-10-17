b81ec63ac33b3a00cdbb094310937f77e1ccbaa7
Davi Nascimento <davi.nascimento777@gmail.com>
2019-10-17 03:36:26 -0300

    Nova atualização
    Mudanças na barra de navegação e no botão de enviar inscrição

--- javascript/scriptJS.js
+++ javascript/scriptJS.js
@@ -1,15 +1,15 @@
-   function loginA(){
-    
-    var username=document.getElementById('uname').value;
-    username=username.toLowerCase();
-    var password=document.getElementById('psw').value;
-    password=password.toLowerCase();
-    
+   $ = require('./jquery-3.4.1')
 
-    if (username=="admin" && password=="admin") { 
-        window.location.replace("./admin.html");}
-    else if (username=="outro" && password=="outrosenha") { window.location.href="./admin.html";  }
-    else if (username=="outro" && password=="outrasenha") {  window.location.href="./admin.html"; }
-    else { alert("Senha ou Usuário inválido."); }
-    
-    }
+   function loginA() {
+
+       var username = document.getElementById('uname').value;
+       username = username.toLowerCase();
+       var password = document.getElementById('psw').value;
+       password = password.toLowerCase();
+
+
+       if (username == "admin" && password == "admin") {
+           window.location.replace("./admin.html");
+       } else if (username == "outro" && password == "outrosenha") { window.location.href = "./admin.html"; } else if (username == "outro" && password == "outrasenha") { window.location.href = "./admin.html"; } else { alert("Senha ou Usuário inválido."); }
+
+   }
